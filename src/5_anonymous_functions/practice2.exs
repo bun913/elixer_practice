6e9ffa947cf21133775126e9b8b8eee0e3c17ecb
bun913 <imanau03@gmail.com>
2021-10-20 08:41:59 +0900

    Add: 5_3の練習問題を回答

--- src/5_anonymous_functions/practice2.exs
+++ src/5_anonymous_functions/practice2.exs
@@ -5,7 +5,11 @@ fizzbuzz = fn
   _, _, c -> c
 end
 
-IO.puts(fizzbuzz.(0, 0, 1))
-IO.puts(fizzbuzz.(0, 1, 1))
-IO.puts(fizzbuzz.(1, 0, 1))
-IO.puts(fizzbuzz.(1, 1, 5))
+# IO.puts(fizzbuzz.(0, 0, 1))
+# IO.puts(fizzbuzz.(0, 1, 1))
+# IO.puts(fizzbuzz.(1, 0, 1))
+# IO.puts(fizzbuzz.(1, 1, 5))
+
+nfizz = fn n -> fizzbuzz.(rem(n, 3), rem(n, 5), n) end
+
+for n <- [10, 11, 12, 13, 14, 15, 16], do: IO.puts(nfizz.(n))
